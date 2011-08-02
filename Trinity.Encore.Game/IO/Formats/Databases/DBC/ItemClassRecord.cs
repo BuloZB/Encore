f77a78fb2cebc24ecb3b8e2d02b3a187e37f6ca0
Machiavelli <machiavelli.trinity@gmail.com>
2011-08-02 03:34:48 +0200

    Teeny tiny research on ItemReforgeRecord and ItemClassRecord dbc formats

---
+++ Trinity.Encore.Game/IO/Formats/Databases/DBC/ItemClassRecord.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace Trinity.Encore.Game.IO.Formats.Databases.DBC
+{
+    [ContractVerification(false)]
+    public sealed class ItemClassRecord : IClientDbRecord
+    {
+        public int Id { get; set; }
+
+        public int ClassId { get; set; }
+
+        public int SubClassId { get; set; }     // or InventoryType
+
+        public int Unk1 { get; set; }           // Unk, sometimes this >> SubclassID & 1 is checked
+
+        public int Unk2 { get; set; }           // Unk
+    }
+}
---
+++ Trinity.Encore.Game/IO/Formats/Databases/DBC/ItemReforgeRecord.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace Trinity.Encore.Game.IO.Formats.Databases.DBC
+{
+    [ContractVerification(false)]
+    public sealed class ItemReforgeRecord : IClientDbRecord
+    {
+        public int Id { get; set; }
+
+        public int Unk1 { get; set; }
+
+        public int Unk2 { get; set; }
+
+        public int Unk3 { get; set; }
+    }
+}
