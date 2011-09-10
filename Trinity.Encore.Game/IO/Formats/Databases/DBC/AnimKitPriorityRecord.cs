c57d334f69b721d2231a06e60d07ce9370625352
Nay <dnpd.dd@gmail.com>
2011-09-10 04:41:32 +0100

    Add structure for AnimKitPriority

---
+++ Trinity.Encore.Game/IO/Formats/Databases/DBC/AnimKitPriorityRecord.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace Trinity.Encore.Game.IO.Formats.Databases.DBC
+{
+    [ContractVerification(false)]
+    public sealed class AnimKitPriorityRecord : IClientDbRecord
+    {
+        // Verified in 14545
+
+        public int Id { get; set; }
+
+        public int Priority { get; set; }
+    }
+}
